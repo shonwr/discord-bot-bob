936b7ae36082ae3a5515fd1084530ceeb2f40c34
Bobthebuilder30 <35590200+Bobthebuilder30@users.noreply.github.com>
2018-01-19 07:08:47 -0800

    Added BlessRNG command

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import discord
 from discord.ext.commands import Bot
 from urllib.parse import urlencode
 import secrets
+import aiohttp
 import asyncio
 import time
+import os
 import sys
+import random
 
 
 bob_the_builder = Bot(command_prefix="!")
@@ -26,5 +29,10 @@ async def youtube(*args):
     print(args)
     url = ("https://www.youtube.com/results?{}".format(urlencode({'search_query': ' '.join(args)})))  
     return await bob_the_builder.say("Got'em! Here are your results : \n \n %s" % url)
+
+@bob_the_builder.command(pass_context=True)
+async def blessrng(ctx):
+	x = random.randint(1, 100)
+	await bob_the_builder.send_message(ctx.message.channel, "<:BlessRNG:403833576313061387>\n \n" + "RNGesus gives you a " + str(x))
 
 bob_the_builder.run(secrets.BOT_TOKEN)
